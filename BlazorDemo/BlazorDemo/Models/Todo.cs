fe6adc1bbb78a6cdf984ea6adf02206b1b79df67
sina wahed <sina.wahed@gmail.com>
2021-01-29 08:47:41 +0100

    Added Blazor demo solution

---
+++ BlazorDemo/BlazorDemo/Models/Todo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorDemo.Models
+{
+    public class Todo
+    {
+        public string Title { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
